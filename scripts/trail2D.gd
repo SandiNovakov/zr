f7cd66ee6eeec124ca368e27857850bbfec2faa7
SandiNovakov <novakov.sandi@gmail.com>
2026-04-04 19:37:17 +0200

    fix Trail2D not respecting physics_interpolation on/off. Added new commands for TPS and physics_interpolation in debug_console.

--- scripts/trail2D.gd
+++ scripts/trail2D.gd
@@ -60,10 +60,13 @@ func _physics_process(delta: float) -> void:
     curr_pos = parent.position
 
 func _process(delta: float) -> void:
-    var alpha := Engine.get_physics_interpolation_fraction()
-    var interpolated_pos := prev_pos.lerp(curr_pos, alpha)
-    
-    queue.push_front(interpolated_pos)
+    if get_tree().physics_interpolation:
+        var alpha := Engine.get_physics_interpolation_fraction()
+        var interpolated_pos := prev_pos.lerp(curr_pos, alpha)
+        
+        queue.push_front(interpolated_pos)
+    else:
+        queue.push_front(curr_pos)
     
     if queue.size() > max_length:
         queue.pop_back()
